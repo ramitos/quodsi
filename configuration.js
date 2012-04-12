85137515ef0d4ea861f8d945d7293f9b7b390c87
Sergio Ramos <ramitos@MacBook-de-Sergio-Ramos.local>
2012-04-12 02:25:31 +0100

    alfred transition completed, arca not woking

--- configuration.js
+++
@@ -1,9 +0,0 @@
-var file = require('file');
-
-module.exports = {
-	'engine': 'dropbox',
-	'link': 'https://www.dropbox.com/s/i3nu0semiguzh98',
-	'path': file.path.join(__dirname, 'articles'),
-	'database': file.path.join(__dirname, 'db'),
-	'filetypes': '\.md|\.markdown'
-}
